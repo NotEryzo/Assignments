bca0a267713ff5af37482a52177933efa6e42c28
NotEryzo <119642792+NotEryzo@users.noreply.github.com>
2023-04-18 14:41:43 -0600

    Add files via upload

--- spellcheck-python-startcode-main/spellcheck.py
+++ spellcheck-python-startcode-main/spellcheck.py
@@ -4,7 +4,9 @@
 # 2: aliceWords: a list containing all of the words from "AliceInWonderland.txt"
 
 import re  # Needed for splitting text with a regular expression
-import time
+import time 
+
+# Linear and Binary functions
 
 def linearsearch(anArray, item):
     for i in range (len(anArray)):
@@ -27,10 +29,16 @@ def binarysearch(anArray, item):
 
     return -1
 
+# ---------------------------------------------------------------
+
+# Main program function 
+
 def main():
     # Load data files into lists
-    dictionary = loadWordsFromFile("spellcheck-python-startcode-main/data-files/dictionary.txt")
-    aliceWords = loadWordsFromFile("spellcheck-python-startcode-main/data-files/AliceInWonderLand.txt")
+    dictionary = loadWordsFromFile("Assignments-main\spellcheck-python-startcode-main/data-files/dictionary.txt")
+    aliceWords = loadWordsFromFile("Assignments-main\spellcheck-python-startcode-main/data-files/AliceInWonderLand.txt")
+
+    # Loop Menu
 
     while True:
         print("\nMain Menu")
@@ -40,9 +48,11 @@ def main():
         print("4. Spell Check Alice In Wonderland (Binary Search)")
         print("5. Exit")
         choice = int(input("Enter menu selection: "))
-        word = input("Please enter a word: ")
+
+        # Find word using Linear Search
 
         if choice == 1:
+            word = input("Please enter a word: ").lower()
             timeStart = time.time()
             output = linearsearch(dictionary, word)
             timeEnd = time.time()
@@ -53,7 +63,11 @@ def main():
             else:
                 print(f"{word} is not in the dictionary. ({seconds} seconds)")
 
-        if choice == 2:
+
+        # Find word using Binary Search
+
+        elif choice == 2:
+            word = input("Please enter a word: ").lower()
             timeStart = time.time()
             output = binarysearch(dictionary, word)
             timeEnd = time.time()
@@ -63,7 +77,47 @@ def main():
                 print(f"{word} is in the dictionary at position {output}. ({seconds} seconds)")
             else: 
                 print(f"{word} is not in the dictionary. ({seconds} seconds)")
+        
+        # How many words are not found using Linear Search
 
+        elif choice == 3:
+            word = input("Please enter a word: ").lower()
+            print("\nLinear Search starting...")
+            count = 0
+            timeStart = time.time()
+            for i in range(len(aliceWords)):
+                output = linearsearch(dictionary, aliceWords[i])
+                if output == - 1:
+                    count += 1
+            timeEnd = time.time()
+            seconds = timeEnd - timeStart
+            print(f"The total number of words not found is: {count}. ({seconds} seconds)")
+
+        # How many words are not found using Binary Search
+
+        elif choice == 4:
+            word = input("Please enter a word: ").lower()
+            print("\nBinary Search starting...")
+            count = 0
+            timeStart = time.time()
+            for i in range(len(aliceWords)):
+                output = linearsearch(dictionary, aliceWords[i])
+                if output == -1:
+                    count += 1
+            timeEnd = time.time()
+            seconds = timeEnd - timeStart
+            print(f"The total number of words not found is: {count}. ({seconds} seconds)")
+
+        # End Menu Loop 
+
+        elif choice == 5:
+            break
+
+        # Invalid Input 
+
+        else :
+            print("\nPlease select a number between (1-5)")
+        
 
 # end main()
 
